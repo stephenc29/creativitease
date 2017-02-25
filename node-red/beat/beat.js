5ca7c6e3a91f24a310c6fa49618a01dcab2b99fa
dcs0spb <s.p.bradley@durham.ac.uk>
2017-02-25 21:18:01 +0000

    Beats per minute

--- node-red/beat/beat.js
+++ node-red/beat/beat.js
@@ -3,20 +3,40 @@ module.exports = function(RED) {
 //    var NanoTimer = require('nanotimer');
 //    var timer = new NanoTimer();
     var tick ;
+    var context;
     
     function BeatNode(config) {
 	
         RED.nodes.createNode(this,config);
         var node = this;
+	context = this.context();
+	context.set("bpm", 100);
         this.on('input', function(msg) {
-	    if(msg.payload=="start"){
-		tick = setInterval(beat, 1000);
+	    switch(msg.payload){
+	    case "start":
+		beat();
+		break;
+
+	    case "stop":
+		clearTimeout(tick);
+		break;
+
+	    case "interval":
+		var bpm = msg.bpm;
+		if(!isNaN(bpm)){
+		    if(bpm>10 && bpm <400){
+			context.set("interval", 60000.0/bpm);
+		    }
+		}
+		break;
 	    }
         });
 
 	function beat(){
 	    var msg = {payload:"beat"};
             node.send(msg);
+	    var interval = context.get("interval");
+	    tick = setTimeout(beat, interval);
 	}
 
     }
