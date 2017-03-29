19fb381c78021824d5c98761e92a579f922b7feb
dcs0spb <s.p.bradley@durham.ac.uk>
2017-03-30 01:47:33 +0800

    allow configuration of looping and restart

--- node-red/beat-divider/sequencer.js
+++ node-red/beat-divider/sequencer.js
@@ -1,12 +1,16 @@
-// allow restart on tick
-// enable/disable looping
-// start array includes note
+// integrate synthesiser: configure with instrument? Think about how this would work with switch block for drums. Maybe synth and a subflow.
+// allow volume control (and add a subflow with a volume slider)
 // fancy parsing of notes e.g. [1,1^,+3]*8 and note names
 // sequencing loudness
 // validity check in property editing
 // check that notes and rhythm are arrays of ints
 // allow notes or rhythm to be singleton number instead of singleton list
 // default and non-default scale
+// section sequencing from named list: separate node?
+// balance volumes of synths
+// route synths through effects
+// multiple sequencers working with the same synth: concurrent sounds
+// chords
 
 module.exports = function(RED) {
     "use strict";
@@ -39,19 +43,25 @@ module.exports = function(RED) {
 	}
 
 	node.octave = config.octave || 0;
+
+	node.start = config.start || "bar"; // sequence won't start until this
+
+	node.loop = config.loop || false;
 	
 	node.root = 60; // make this configurable as a name or offset, middle C for now
 	node.mode = "minor"; // make this configurable
-	
-	node.rhythmPos = -1; // the position in the list of lengths
-	node.rhythmCount = 0; // count down the number of beats
-	node.notePos = -1; // the position in the list of notes
 
+	reset();
+	
         this.on('input', function(msg) {
 	    switch(msg.payload){
 	    case "tick":
 		var start = msg.start || [];
 		if(start.indexOf(node.input)>=0){
+//		    only start/ restart when we get the right kind of tick
+		    if(node.rhythmPos == -1 &&start.indexOf(node.start)<0){
+			return;
+		    }
 		    node.rhythmCount--;
 		    if(node.rhythmCount<=0){
 			node.rhythmPos++;
@@ -63,7 +73,13 @@ module.exports = function(RED) {
 
 			node.notePos++;
 			if(node.notePos >= node.notes.length){
-			    node.notePos = 0;
+			    if(node.loop){
+				node.notePos = 0;
+			    }
+			    else{
+				reset();
+				return;
+			    }
 			}
 			msg.count["note_pos"] = node.notePos;
 			msg.note_midi = note2midi(node.notes[node.notePos], node);
@@ -79,6 +95,12 @@ module.exports = function(RED) {
 		node.send(msg);
 	    }
         });
+
+	function reset(){
+	    node.rhythmPos = -1; // the position in the list of lengths
+	    node.rhythmCount = 0; // count down the number of beats
+	    node.notePos = -1; // the position in the list of notes
+	}
 
 
     }
